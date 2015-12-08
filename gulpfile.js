2fe6aa3863f72e57678d658785d5777f7c407fe2
FreemanVitali <aiderbox@gmail.com>
2015-12-08 22:46:46 +0300

    Ready proj2

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 var gulp = require("gulp"),
-browserSync = require('browser-sync');
-gulp.task('server', function () {
-browserSync({
-port: 9000,
-server: {
-baseDir: 'Ls-web'
-}
-});
-});
+    browserSync = require('browser-sync');
+
+gulp.task('server', function () {  
+  browserSync({
+    port: 9000,
+    server: {
+      baseDir: 'Ls-web'
+    }
+  });
+});
+
 gulp.task('watch', function () {
-gulp.watch([
-'app/*.html',
-'app/js/**/*.js',
-'app/css/**/*.css'
-]).on('change', browserSync.reload);
-});
-gulp.task('default', ['server', 'watch']);
+  gulp.watch([
+  	'Ls-web/*.css',
+    'Ls-web/*.html',
+    'Ls-web/js/**/*.js',
+    'Ls-web/css/**/*.css'
+  ]).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['server', 'watch']);
