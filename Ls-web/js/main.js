2661e86d531777b368c4928accee00358c3f0118
FreemanVitali <aiderbox@gmail.com>
2015-12-11 19:31:44 +0300

    Poup window on js

--- Ls-web/js/main.js
+++ Ls-web/js/main.js
@@ -1,1 +1,11 @@
-var = test
+var popupLink = document.getElementById("add-projekt-bg");
+var popupCss = document.getElementById("popup-back");
+
+popupLink.onclick = function(){
+popupCss.style.visibility = "visible";
+};
+// function popup () {
+//  $('.popup-back').css('display','inherit');
+//  $('.monster').css('display','none');
+//  $("#t2-list_copy").text($("#t2-list").text());
+// }
