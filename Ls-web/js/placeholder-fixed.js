5ee7922b53b9965445ed0f4a630569a499d0aba7
FreemanVitali <aiderbox@gmail.com>
2015-12-21 14:17:45 +0300

    fix IE8

---
+++ Ls-web/js/placeholder-fixed.js
@@ -0,0 +1,5 @@
+//Fixed place-hokder for IE8
+
+var placeholder = (function()  {
+	$('.form-url, .filename, .form-message, .form-name, form-message, .form-email').placeholder();
+	})();
---
+++ Ls-web/js/placeholder-ie8.js
@@ -0,0 +1,6 @@
+//Fixed place-hokder for IE8
+
+if (typeof console === "undefined" || typeof console.log === "undefined") {
+     console = {};
+     console.log = function() {};
+ }
